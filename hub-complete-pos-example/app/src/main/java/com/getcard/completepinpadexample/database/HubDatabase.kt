232c3ac74f3747a8e65d1c9dbc5c6f09e11b81a0
Leonardo Dalberto <leonardo.dalberto@sgsistemas.com.br>
2025-03-21 16:57:08 -0300

    feat: add complete pos example

---
+++ hub-complete-pos-example/app/src/main/java/com/getcard/completepinpadexample/database/HubDatabase.kt
@@ -0,0 +1,48 @@
+package com.getcard.completepinpadexample.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.getcard.completepinpadexample.database.daos.HubSettingsDao
+import com.getcard.completepinpadexample.database.daos.SitefSettingsDao
+import com.getcard.completepinpadexample.database.daos.TransactionsDao
+import com.getcard.completepinpadexample.database.models.HubSettingsModel
+//import com.getcard.completepinpadexample.database.models.ScopeSettingsModel
+import com.getcard.completepinpadexample.database.models.SitefSettingsModel
+import com.getcard.completepinpadexample.database.models.TransactionsModel
+
+@Database(
+    entities = [HubSettingsModel::class, /*ScopeSettingsModel::class,*/ SitefSettingsModel::class, TransactionsModel::class],
+    version = 1,
+    exportSchema = true,
+    autoMigrations = [
+//        AutoMigration(from = 1, to = 2),
+//        AutoMigration(from = 2, to = 3)
+    ]
+)
+@TypeConverters(DatabaseConverters::class)
+abstract class HubDatabase : RoomDatabase() {
+    abstract fun settingsDao(): HubSettingsDao
+    abstract fun sitefSettingsDao(): SitefSettingsDao
+    abstract fun transactionsDao(): TransactionsDao
+//    abstract fun scopeSettingsDao(): ScopeSettingsDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: HubDatabase? = null
+
+        fun getInstance(context: Context): HubDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    HubDatabase::class.java, "payment_hub"
+                )
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+
+}
